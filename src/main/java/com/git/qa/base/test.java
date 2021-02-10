cf6ff24f8dff41d9d041fa9bd5845cd65777c31e
mrinmoymaji6 <www.engineermrinmoy@gmail.com>
2021-02-10 15:03:37 +0530

    first git commit

---
+++ src/main/java/com/git/qa/base/test.java
@@ -0,0 +1,8 @@
+package com.git.qa.base;
+
+public class test {
+	public test(){
+		System.out.println("Inside Test");
+	}
+	
+}
---
+++ src/test/java/com/git/qa/base/NewTest.java
@@ -0,0 +1,9 @@
+package com.git.qa.base;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
